641acbc8dcfea21f8e863c978abd737dd6a6be70
Josde <jorgecruz@usal.es>
2021-04-23 15:01:03 +0200

    Added basic CSS, added difference from start, sort by difference.

--- tracker/views.py
+++ tracker/views.py
@@ -1,27 +1,27 @@
 import os
 from datetime import datetime
-
+from .tables import LeagueTable
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import riotwatcher
-from tracker.models import LeagueData, MiscData
+from tracker.models import LeagueData
 from django.views.generic import ListView
-
-class LeagueDataListView(ListView):
+from django_tables2 import SingleTableView
+from .misc import *
+class LeagueDataListView(SingleTableView):
     model = LeagueData
+    table_class = LeagueTable
     template_name = 'tracker/tracker.html'
+    ordering = "-progress"
 
     def get_context_data(self, **kwargs):
         ctx = super(LeagueDataListView, self).get_context_data(**kwargs)
-        ctx['date'] = MiscData.objects.first().date;
+        ctx['date'] = datetime.now;
         return ctx
 
+
+
 def index(request):
-    if MiscData.objects.count() == 0:
-        MiscData.objects.create(date=datetime.now)
-    else:
-        MiscData.objects.first().delete();
-        MiscData.objects.create(date=datetime.now)
     if (request.method == 'POST'):
         for x in LeagueData.objects.all().iterator():
             x.delete()
@@ -30,17 +30,24 @@ def index(request):
     else:
         return redirect('tracker', permanent=False)
 
+
 def updateData():
     lolWatcher = riotwatcher.LolWatcher(os.environ.get("API_KEY"))
-    region = os.environ.get("REGION");
-    playernames = os.environ.get("PLAYERS").split(",");
-    for str in playernames:
-        summonerData = lolWatcher.summoner.by_name(region, str);
+    region = os.environ.get("REGION")
+    playernames = os.environ.get("PLAYERS").split(",")
+    startingRanks = os.environ.get("STARTING_RANKS").split(",")
+    for str, str2 in zip(playernames, startingRanks):
+        summonerData = lolWatcher.summoner.by_name(region, str)
         allLeagueData = lolWatcher.league.by_summoner(region, summonerData['id'])
-
+        startingRankSplit = list(filter(None, str2.split(" ")))
         for element in allLeagueData:
             if (element['queueType'] == "RANKED_SOLO_5x5"):
-                leagueData = element;
+                leagueData = element
+                winrate = float(leagueData['wins']) / (leagueData['wins'] + float(leagueData['losses']))
+                startingLP = rankToLP(startingRankSplit[0], startingRankSplit[1], startingRankSplit[2])
+                currentLP = rankToLP(leagueData['tier'], leagueData['rank'], leagueData['leaguePoints'])
                 LeagueData.objects.create(name=summonerData['name'], tier=leagueData['tier'], rank=leagueData['rank'],
                                           points=leagueData['leaguePoints'], wins=leagueData['wins'],
-                                          losses=leagueData['losses'])
+                                          losses=leagueData['losses'], winrate=winrate, progress=currentLP-startingLP)
+
+
