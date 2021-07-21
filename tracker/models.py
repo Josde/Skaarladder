c7c335c16183bbec682194c5fa53f8ba4c8cb602
Josde <jorgecruz@usal.es>
2021-07-22 00:00:35 +0200

    added defaults and choices to make it easier to configure the model.

--- tracker/models.py
+++ tracker/models.py
@@ -20,16 +20,40 @@ class LeagueData(models.Model):
 # TODO: Add ChallengeList to either this or LeagueData so we can have multiple leaderboards. For now, Challenge is a glorified way to store a date.
 # Using Junction tables to store lists: https://stackoverflow.com/a/444268
 class TrackedPlayers(models.Model):
+    regionChoices = [('EUW', 'EUW1'),
+                     ("BR","BR1"),
+                     ("EUNE", "EUN1"),
+                     ('JP', 'JP1'),
+                     ('KR', 'KR1'),
+                     ('LAN', 'LA1'),
+                     ('LAS', 'LA2'),
+                     ('OCE', 'OC1'),
+                     ('RU', 'RU'),
+                     ('TR', 'TR1')]
+    tierChoices = [('IRON', 'IRON'),
+                   ('BRONZE', 'BRONZE'),
+                   ('SILVER', 'SILVER'),
+                   ('GOLD', 'GOLD'),
+                   ('PLATINUM', 'PLATINUM'),
+                   ('DIAMOND', 'DIAMOND')]
+    rankChoices = [('I', 'I'),
+                   ('II', 'II'),
+                   ('III', 'III'),
+                   ('IV', 'IV')]
+
     name = models.CharField(max_length=100, primary_key=True)
-    startingTier = models.CharField(max_length=30)
-    startingRank = models.CharField(max_length=10)
+    #Defaults set just to prevent ------ to show up in choices.
+    startingTier = models.CharField(max_length=30, choices=tierChoices, default='SILVER')
+    startingRank = models.CharField(max_length=10, choices=rankChoices, default='I')
     startingPoints = models.IntegerField()
-    region = models.CharField(max_length=10)
-    ignored = models.BooleanField()
+    region = models.CharField(max_length=10, choices=regionChoices, default='EUW')
+    ignored = models.BooleanField(default=False)
 
 class Challenge(models.Model):
+    queueTypeChoices = [('SoloQ', 'RANKED_SOLO_5x5'),
+                 ('Flex', 'RANKED_FLEX_5x5')]
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     endDate = models.DateField()
-    queueType = models.CharField(max_length=25)
+    queueType = models.CharField(max_length=25, choices=queueTypeChoices, default='RANKED_SOLO_5x5')
 
