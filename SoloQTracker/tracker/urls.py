3727ad97cad71986b67f332223a369d9c8d603fd
Josde <jorgecruz@usal.es>
2022-07-14 22:22:03 +0200

    Add test view for debugging

--- SoloQTracker/tracker/urls.py
+++ SoloQTracker/tracker/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('player/<str:player>', views.player, name='player'),
 ]
--- SoloQTracker/tracker/views.py
+++ SoloQTracker/tracker/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
+from tracker.updater.update_helper import UpdateHelper
+from tracker.models import Player
 # Create your views here.
 def index(request): 
     template = loader.get_template('tracker/tracker.html')
     return HttpResponse(template.render())
 
+async def player(request, player=""):
+    if (player != ""):
+        ourPlayer = Player.create(player, 'euw1')
+        uh = UpdateHelper(ourPlayer)
+        await uh.update()
+    return HttpResponse('')
