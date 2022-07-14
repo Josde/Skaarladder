f3bb3254e77e699fd3d4654a0a64a5c0ba58e601
Josde <jorgecruz@usal.es>
2022-07-14 22:21:26 +0200

    Add basic model

--- SoloQTracker/tracker/admin.py
+++ SoloQTracker/tracker/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from tracker.models import Player, Challenge_Player, Challenge
 # Register your models here.
+admin.site.register([Player, Challenge_Player, Challenge])
--- SoloQTracker/tracker/models.py
+++ SoloQTracker/tracker/models.py
@@ -1,7 +1,90 @@
+from logging.handlers import SYSLOG_UDP_PORT
+from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
+from pyot.utils.lol.routing import platform_to_region
+from asgiref.sync import sync_to_async
+# Choices for ease of use on choice fields. Could be stored in constants.py, but isn't done since usage is constrained to this file for now.
+regionChoices = [('euw1', 'euw1'),
+                     ("br1","br1"),
+                     ("eun1", "eun1"),
+                     ('jp1', 'jp1'),
+                     ('kr1', 'kr1'),
+                     ('la1', 'la1'),
+                     ('la2', 'la2'),
+                     ('oc1', 'oc1'),
+                     ('ru', 'ru'),
+                     ('tr1', 'tr1')]
 
+platformChoices = [('europe', 'europe'),
+                     ("americas","americas"),
+                     ("asia", "asia"),
+                     ]
+
+tierChoices = [('IRON', 'IRON'),
+                ('BRONZE', 'BRONZE'),
+                ('SILVER', 'SILVER'),
+                ('GOLD', 'GOLD'),
+                ('PLATINUM', 'PLATINUM'),
+                ('DIAMOND', 'DIAMOND'),
+                ('MASTER_PLUS', 'MASTER_PLUS')]
+rankChoices = [('I', 'I'),
+                ('II', 'II'),
+                ('III', 'III'),
+                ('IV', 'IV')]
 
 class User(AbstractUser): # Override user model for future use, just in case. See: https://docs.djangoproject.com/en/4.0/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
     pass 
+
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    puuid = models.CharField(max_length=100, default="")
+    summoner_id = models.CharField(max_length=100, default="")
+    account_id = models.CharField(max_length=100, default="")
+    region = models.CharField(max_length=10, default='euw1', choices=regionChoices)
+    platform = models.CharField(max_length=10, default='europe', choices=platformChoices)
+    last_data_update = models.DateTimeField(default=timezone.now)
+    # Ranked data
+    tier = models.CharField(max_length=100, default='SILVER', choices=tierChoices)
+    rank = models.CharField(max_length=5, default='I', choices=rankChoices)
+    lp = models.IntegerField(default=0)
+    wins = models.IntegerField(default=0)
+    losses = models.IntegerField(default=0)
+    winrate = models.FloatField(default=0)
+    streak = models.IntegerField(default=0)
+
+    @classmethod
+    def create(cls, name, platform):
+        res = cls(name=name, platform=platform, region=platform_to_region(platform))
+        return res
+    def __str__(self): # For admin panel aesthetics
+        return self.name
+#TODO: Implement django-model-history so I can get stadistics out of this model.
+
+class Challenge(models.Model):
+    name = models.CharField(max_length=100)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    is_absolute = models.BooleanField()
+    last_access_date = models.DateTimeField(default=timezone.now)
+
+
+class Challenge_Player(models.Model):
+    # Foreign keys to relate a challenge and a player 
+    player_id = models.ForeignKey(Player, on_delete=models.CASCADE)
+    challenge_id = models.ForeignKey(Challenge, on_delete=models.CASCADE)
+    # Starting state and settings.
+    starting_rank = models.CharField(max_length=20, choices=rankChoices) 
+    starting_tier = models.CharField(max_length=30, choices=tierChoices) # Doesn't use ChoiceField for now since this has to be updated programmatically.
+    starting_lp = models.IntegerField()
+    ignored = models.BooleanField(default=False)
+    # Current state of the player
+    progress = models.IntegerField(default=0)
+    progress_delta = models.IntegerField(default=0)
+    last_update = models.DateTimeField(default=timezone.now)
+    class Meta:
+        indexes = [
+            models.Index(fields=['player_id', 'challenge_id'])
+        ]
+    # TODO: Check out index_together for efficiency. See: https://docs.djangoproject.com/en/4.0/ref/models/options/#index-together
+    
