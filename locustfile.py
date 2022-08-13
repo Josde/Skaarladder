d5aa3ff678b072ac76b96e5e92e95b0a63b9e158
Josde <jorgecruz@usal.es>
2022-08-13 21:06:52 +0200

    remove unused files

--- locustfile.py
+++
@@ -1,9 +0,0 @@
-from locust import HttpUser, task
-
-
-class HelloWorldUser(HttpUser):
-    @task
-    def hello_world(self):
-        self.client.get("")
-        self.client.get("ladder/55")
-        self.client.get("error")
--- tracker/utils/validators.py
+++
@@ -1,2 +0,0 @@
-def generic_name_validator(name: str) -> bool:
-    return 3 <= len(name) <= 16
