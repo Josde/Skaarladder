b3fce282ef9377f5e393c3f79bb273b9537850c3
Josde <jorgecruz@usal.es>
2022-08-16 15:21:18 +0200

    fix deployment issues

--- SoloQTracker/settings.py
+++ SoloQTracker/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(config("DEBUG", False))
-
-ALLOWED_HOSTS = []
+# FIXME: Make this not a constant
+ALLOWED_HOSTS = [".josde.me"]
 
 
 # Application definition
